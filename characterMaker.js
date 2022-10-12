4f3a931a0c503d99b2f48abecf009a7fa121e159
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-12 11:53:05 -0600

    first character centered on window

--- characterMaker.js
+++ characterMaker.js
@@ -6,7 +6,11 @@ let buttonLeft = document.getElementById(buttonLeft)
 let buttonRight = document.getElementById(buttonRight)
 
 //Pseudocode guide
-// ----Make buttons
 // ----Make image ‘position: absolute;
 // ----Each button has onclick event, up down left right
 // ----Create function at 2 minute mark of video
+
+//erics thoughts for project
+// ----in positoing abosolute vs relative etc, height refers to the divs containers height in manimpulating the img
+// ----another porperty referencing it's own height called transform>translate(google that shit)
+//----a common one you will see to center absolute thing is 'top 50% transform>translate - 50%' < not accurate code but something like that
