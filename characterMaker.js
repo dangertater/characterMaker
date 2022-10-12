8adce80d567ed5896e11a7b209eaaf5330396dad
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-12 11:04:29 -0600

    prior to making characterImg position absolute

---
+++ characterMaker.js
@@ -0,0 +1,12 @@
+//next several lines connect html elements to the JS
+let characterImg = document.getElementById(characterImg)
+let buttonUp = document.getElementById(buttonUp)
+let buttonDown = document.getElementById(buttonDown)
+let buttonLeft = document.getElementById(buttonLeft)
+let buttonRight = document.getElementById(buttonRight)
+
+//Pseudocode guide
+// ----Make buttons
+// ----Make image ‘position: absolute;
+// ----Each button has onclick event, up down left right
+// ----Create function at 2 minute mark of video
