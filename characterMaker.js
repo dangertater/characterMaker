b0d0a29681a389e6e769384ee3ee1bd891fc8cff
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-13 09:48:14 -0600

    jsut starting for the morning

--- characterMaker.js
+++ characterMaker.js
@@ -1,3 +1,4 @@
+//next several lines connect html elements to the JS
 let characterImg = document.getElementById("characterImg")
 let buttonUp = document.getElementById("buttonUp")
 let buttonDown = document.getElementById("buttonDown")
@@ -6,19 +7,64 @@ let buttonRight = document.getElementById("buttonRight")
 let currentTopVal = characterImg.style.top
 let currentBorderColor = characterImg.style.border
 let selectedRow = 0
+let selectedColumn = 0
 let rowHeight = 50
+let columnWidth = 47
 let style = getComputedStyle(characterImg)
-let removePxString = style.top.replace("px", "")
+// let removeTopPx = style.top.replace("px", "")
+// let removeLeftPx = style.top.replace("px", "")
+
+//cycles the user back to top/bottom/left/right if they try to select a char that is off grid
+let cycleBack = () => {
+	let removeTopPx = style.top.replace("px", "")
+	let removeLeftPx = style.left.replace("px", "")
+	if (Number(removeTopPx) > 340) {
+		characterImg.style.top = `190px`
+	} else if (Number(removeTopPx) < 190) {
+		characterImg.style.top = `340px`
+	} else if (Number(removeLeftPx) > 562) {
+		characterImg.style.left = `327px`
+	} else if (Number(removeLeftPx) < 327) {
+		characterImg.style.left = `562px`
+	}
+}
+
+// less than 327
+// greater than 562
+//TODO all four buttons could have global variables instead, will need to make it a functiont
+//----to allow for the 'onClick'to occur. otherwise the variables (and assosciated functions)
+//----will only be triggered once on initialization\
 
 buttonUp.addEventListener("click", (e) => {
-	let top = Number(removePxString)
-	characterImg.style.cssText = `top: ${top - 50}px`
+	let removePx = style.top.replace("px", "")
+	let top = Number(removePx)
+	characterImg.style.top = `${top - rowHeight}px`
+	cycleBack()
 })
 
 buttonDown.addEventListener("click", (e) => {
-	let top = Number(removePxString)
-	characterImg.style.cssText = `top: ${top + 50}px`
+	let removePx = style.top.replace("px", "")
+	let top = Number(removePx)
+	characterImg.style.top = `${top + rowHeight}px`
+	cycleBack()
 })
+
+buttonLeft.addEventListener("click", (e) => {
+	let removePx = style.left.replace("px", "")
+	let left = Number(removePx)
+	characterImg.style.left = `${left - columnWidth}px`
+	cycleBack()
+})
+
+buttonRight.addEventListener("click", (e) => {
+	let removePx = style.left.replace("px", "")
+	let left = Number(removePx)
+	characterImg.style.left = `${left + columnWidth}px`
+	cycleBack()
+})
+
+//remove px from string
+//use capital N to convert string into a number
 
 // characterImg.style.cssText = `top: ${selectedRow * rowHeight}px`
 //this code switched the img file to original locationb ack and forth forever
